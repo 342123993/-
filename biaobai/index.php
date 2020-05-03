6f73030c95bea67c925cac2a7c899b2a680d8fa5
342123993 <61654408+342123993@users.noreply.github.com>
2020-05-03 17:00:27 +0800

    Add files via upload

---
+++ biaobai/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>表白网页模板</title>
+</head>
+<body>
+
+ <embed src=http://www.kugou.com/song/y6ccv7d.html?from_sogou#hash=47EE67D40C36A3457EC39D6C7166AC63&album_id=0 width=0 height=0 type=audio/mpeg autostart="false">
+<p align="center"><a href="./1/index.html" target="_blank">一个不完整的表白网页1</a></p>
+
+<p align="center"><a href="./2/index.htm" target="_blank">一个不完整的表白网页2</a></p>
+
+<p align="center"><a href="./3/index.html" target="_blank">一个不完整的表白网页3</a></p>
+
+<p align="center"><a href="./4/index.html" target="_blank">一个不完整的表白网页4</a></p>
+
+<p align="center"><a href="./5/index.asp" target="_blank">一个不完整的表白网页5</a></p>
+
+<p align="center"><a href="./6/index.htm" target="_blank">一个不完整的表白网页6</a></p>
+
+<p align="center"><a href="./7/index.html" target="_blank">一个不完整的表白网页7</a></p>
+
+<p align="center"><a href="./8/index.html" target="_blank">一个不完整的表白网页8</a></p>
+
+<p align="center"><a href="./9/index.html" target="_blank">一个不完整的表白网页9</a></p>
+
+<p align="center"><a href="./10/index.html" target="_blank">一个不完整的表白网页10</a></p>
+
+<p align="center"><a href="./10/index2.html" target="_blank">一个不完整的表白网页11</a></p>
+
+<p align="center"><a href="./11/index.html" target="_blank">一个不完整的表白网页12</a></p>
+
+<p align="center"><a href="./12/index.html" target="_blank">一个不完整的表白网页13</a></p>
+
+<p align="center"><a href="../index.php">返回主页</a></p>
+
+<p align="center">站长qq：1621491832</font></p>
+<p align="center">全是模板，需要源码请联系站长</font></p>
+<script type="text/javascript">document.write(unescape("%3Cspan id='cnzz_stat_icon_1278713356'%3E%3C/span%3E%3Cscript src='https://s4.cnzz.com/z_stat.php%3Fid%3D1278713356%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
+</script>
+</body>
+</html>
