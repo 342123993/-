fc8c6ab2b984770ea384ae7c0e604119171776cd
342123993 <61654408+342123993@users.noreply.github.com>
2020-05-03 16:45:41 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+﻿<!DOCTYPE html>
+<html lang="zh">
+	<head>
+		<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
+       <title>蜡笔大新的网站</title>
+        <meta name="keywords" content="蜡笔大新的网站 yxz.cool" />
+        <meta name="description" content="蜡笔大新的网站 yxz.cool" />
+        <link rel="apple-touch-icon" href="https://cdn.mom1.cn/1/favicon.ico">
+    </head>
+     <body style="padding:12% 5% 5% 5%;background-image:url(style/images/img/bg.jpg);">
+        <div style="text-align:center;border-width:1px;border-style:solid;border-color:rgb(2, 204, 245);background-color:#79918e6b;border-radius:20px;">
+          <p>
+          	<img style="border-radius:50%;max-width:100px;max-height:100px;" src="style/images/QQ图片20200406182343.png"></p>
+             <h1 style="color:#000000;"><strong>蜡笔大新的网站 yxz.cool</strong>
+             </h1>
+           <p style="font-size:3vh;color:#000000;"><font size="6">星河滚烫，你是人间理想；皓月清凉，你是人间曙光</font></p>
+           <p style="line-height:3;">
+           <a style="border-radius:10px;padding:7px 30px;color:#000000;border-style:solid;text-decoration:none;" href="./index.php">首页</a>
+		   <a style="border-radius:10px;padding:7px 30px;color:#000000;border-style:solid;text-decoration:none;" href="./sousuo/index.php">搜索引擎大全</a>
+		   <a style="border-radius:10px;padding:7px 30px;color:#000000;border-style:solid;text-decoration:none;" href="./youqing/index.php">友情链接</a>
+		   <a style="border-radius:10px;padding:7px 30px;color:#000000;border-style:solid;text-decoration:none;" href="./biaobai/index.php">表白网页模板</a>
+		   <a style="border-radius:10px;padding:7px 30px;color:#000000;border-style:solid;text-decoration:none;" href="./jiemi/index.php">解密游戏大全</a>
+            </p><p><script src="https://pv.sohu.com/cityjson?ie=utf-8"></script>
+            <span style="color:#000000;">你的IP:<script>
+                    document.write(returnCitySN["cip"])
+                   </script></span>
+         </p>
+         <p style="color:#000000;">你的地址:<script>
+                    document.write(returnCitySN["cname"])
+                   </script> <a style="text-decoration:none;color:#000000;" </p>
+                   <script type="text/javascript" color="255,255,255" opacity='0.7' zIndex="-2" count="200" src="style/js/canvas-nest.min.js"></script>
+	  <p align="center">站长qq：1621491832</font></p>
+        </div>
+     <script type="text/javascript">document.write(unescape("%3Cspan id='cnzz_stat_icon_1278713356'%3E%3C/span%3E%3Cscript src='https://s4.cnzz.com/z_stat.php%3Fid%3D1278713356%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
+</body>
+</html>
---
+++ jiemi/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="zh">
+	<head>
+		<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
+       <title>解谜游戏</title>
+        <meta name="keywords" content="蜡笔大新的网站 yxz.cool" />
+        <meta name="description" content="蜡笔大新的网站 yxz.cool" />
+        <link rel="apple-touch-icon" href="https://cdn.mom1.cn/1/favicon.ico">
+    </head>
+     <body style="padding:12% 5% 5% 5%;background-image:url(../style/images/img/bg.jpg);">
+        <embed src=http://www.kugou.com/song/y6ccv7d.html?from_sogou#hash=47EE67D40C36A3457EC39D6C7166AC63&album_id=0 width=0 height=0 type=audio/mpeg autostart="false">
+        <div style="text-align:center;border-width:1px;border-style:solid;border-color:rgb(2, 204, 245);background-color:#79918e6b;border-radius:20px;">
+          <p>
+          	<img style="border-radius:50%;max-width:100px;max-height:100px;" src="../style/images/QQ图片20200406182343.png"></p>
+             <h1 style="color:#000000;"><strong>蜡笔大新的网站 yxz.cool</strong>
+             </h1>
+          
+           <a style="border-radius:10px;padding:7px 30px;color:#000000;border-style:solid;text-decoration:none;" href="../index.php">返回首页</a>
+	<a style="border-radius:10px;padding:7px 30px;color:#000000;border-style:solid;text-decoration:none;" href="https://harrynull.tech/cipher/">The Riddle</a>
+	<a style="border-radius:10px;padding:7px 30px;color:#000000;border-style:solid;text-decoration:none;" href="https://harrynull.tech/2020/">New Year Puzzle</a>
+	<a style="border-radius:10px;padding:7px 30px;color:#000000;border-style:solid;text-decoration:none;" href="https://nazo.games/">谜·Games</a>
+	<a style="border-radius:10px;padding:7px 30px;color:#000000;border-style:solid;text-decoration:none;" href="http://nazo.one-story.cn/">Nazo Games</a>
+
+
+
+
+
+
+            
+	</p><p><script src="https://pv.sohu.com/cityjson?ie=utf-8"></script>
+            <span style="color:#000000;">你的IP:<script>
+                    document.write(returnCitySN["cip"])
+                   </script></span>
+         </p>
+         <p style="color:#000000;">你的地址:<script>
+                    document.write(returnCitySN["cname"])
+                   </script> <a style="text-decoration:none;color:#000000;" </p>
+                   <script type="text/javascript" color="255,255,255" opacity='0.7' zIndex="-2" count="200" src="style/js/canvas-nest.min.js"></script>
+	 <p align="center">添加友情链接站长qq：1621491832</font></p>
+        </div>
+     <script type="text/javascript">document.write(unescape("%3Cspan id='cnzz_stat_icon_1278713356'%3E%3C/span%3E%3Cscript src='https://s4.cnzz.com/z_stat.php%3Fid%3D1278713356%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
+</body>
+</html>
